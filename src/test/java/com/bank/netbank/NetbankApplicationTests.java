0af6320fb166a92e5fc74f76561fc30535540593
eli <elijunior.apeles2005@gmail.com>
2024-03-15 14:37:04 -0300

    feat - project created, update md

---
+++ src/test/java/com/bank/netbank/NetbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.netbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
