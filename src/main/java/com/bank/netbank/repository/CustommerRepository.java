c013155c5ed429a2c2ff50dbb8b73c461457ad0c
eli <elijunior.apeles2005@gmail.com>
2024-03-15 19:26:32 -0300

    fix - remove custommer

--- src/main/java/com/bank/netbank/repository/CustommerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bank.netbank.repository;
-
-import com.bank.netbank.model.Custommer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustommerRepository extends JpaRepository<Custommer,Long> {
-}
--- src/main/resources/import.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO  tb_custommer (name, cpf) VALUES ('junior', '9999999999')
